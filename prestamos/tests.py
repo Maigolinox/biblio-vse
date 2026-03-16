4b3f0e1fa39c89593795e4eb71a0a4384dc75650
Victor Terron <vicmictm@hotmail.com>
2026-03-16 16:49:04 -0600

    feat(prestamos): implementa RF_05 con trazabilidad y pruebas

--- prestamos/tests.py
+++ prestamos/tests.py
@@ -1,3 +1,8 @@
+# prestamos/tests.py
 from django.test import TestCase
 
-# Create your tests here.
+class PrestamoTestCase(TestCase):
+    def test_registrar_prestamo_rf05(self):
+        """Valida el RF_05_Gestión_Préstamos"""
+        response = self.client.get('/prestamos/registrar/')
+        self.assertEqual(response.status_code, 200)
