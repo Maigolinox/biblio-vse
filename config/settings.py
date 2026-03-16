26cc13cceb215ac48724278aff2145815aa7cc25
Victor Terron <vicmictm@hotmail.com>
2026-03-16 16:58:03 -0600

    config: agrega password de la base de datos

--- config/settings.py
+++ config/settings.py
@@ -115,3 +115,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+# MAL: Credenciales en texto plano
+DATABASES_PASSWORD = 'super_secret_production_password_2026!'
