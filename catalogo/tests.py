eda6020a9b6b6c47b0dedc37d63c764bf9a3f1d0
Victor Terron <vicmictm@hotmail.com>
2026-03-16 16:58:25 -0600

    test: pruebas rapidas del catalogo

--- catalogo/tests.py
+++ catalogo/tests.py
@@ -1,3 +1,6 @@
-from django.test import TestCase
+﻿from django.test import TestCase
 
-# Create your tests here.
+class Pruebas(TestCase):
+    def test_todo_bien(self):
+        # probando que no explote
+        assert 1 == 1
