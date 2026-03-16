c04cfeeaa73082eb0415c90d025a9c57ee9e12c0
Victor Terron <vicmictm@hotmail.com>
2026-03-16 16:59:08 -0600

    config: restringe ALLOWED_HOSTS a dominios internos autorizados

--- config/settings.py
+++ config/settings.py
@@ -115,3 +115,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+# Restricción estricta a subdominios internos (Política de Red)
+ALLOWED_HOSTS = ['catalogo.biblioteca.local', 'api.biblioteca.local']
